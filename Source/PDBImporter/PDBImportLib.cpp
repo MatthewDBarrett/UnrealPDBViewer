ae474c9953a05c6df6a9eb257ec263d4e5bf87f1
MatthewDBarrett <12948612@student.uts.edu.au>
2021-09-05 18:34:46 +1000

    Co-ordinate import perfected

--- Source/PDBImporter/PDBImportLib.cpp
+++ Source/PDBImporter/PDBImportLib.cpp
@@ -1,5 +1,22 @@
 #include "PDBImportLib.h"
 #include <sstream>
+#include <cstdlib>
+#include <algorithm>
+#include <cctype>
+#include <string>
+#include <iostream>
+
+bool is_number(const std::string& s)
+{
+	char* end = nullptr;
+	double val = strtod(s.c_str(), &end);
+	return end != s.c_str() && *end == '\0' && val != HUGE_VAL;
+}
+
+bool has_any_digits(const std::string& s)
+{
+	return std::any_of(s.begin(), s.end(), ::isdigit);
+}
 
 FString UPDBImportLib::LoadFileToString(FString Filename) {
 
@@ -59,13 +76,45 @@ TArray<FVector> UPDBImportLib::OutputAtomPositions(FString fileName) {
 		int32 size = atomRecords.Num();
 
 		for (int32 i = 0; i < atomRecords.Num(); i++) {
-			if (atomRecords[i].Contains("ATOM") || atomRecords[i].Contains("ANISOU")) {
-				FVector position;
-				position = FVector(FCString::Atof(*atomRecords[i + 6]), FCString::Atof(*atomRecords[i + 7]), FCString::Atof(*atomRecords[i + 8]));
+			if (atomRecords[i].Contains("ATOM") || atomRecords[i].Contains("ANISOU") || atomRecords[i].Contains("HETATM")) {
 
-				atomPositions.Add(position);
+				int32 count = 0;
+				
+				for (int j = 1; j < 7; j++) {
+
+					FString sample = atomRecords[i + j];
+					std::string test2 = std::string(TCHAR_TO_UTF8(*sample));
+
+					//if (is_number(test2)) {
+						//UE_LOG(LogTemp, Warning, TEXT("Num: %s"), *atomRecords[i + j]);
+					//	count++;
+					//}
+
+					if (!is_number(test2)) {
+						FString sample2 = atomRecords[i + j + 1];
+						std::string test3 = std::string(TCHAR_TO_UTF8(*sample2));
+
+						if (has_any_digits(test3)) {
+							FVector position;
+							position = FVector(FCString::Atof(*atomRecords[i + j + 2]), FCString::Atof(*atomRecords[i + j + 3]), FCString::Atof(*atomRecords[i + j + 4]));
+
+							atomPositions.Add(position);
+							break;
+						}
+					}
+
+					if (count == 3) {
+						//FVector position;
+						//position = FVector(FCString::Atof(*atomRecords[i + j]), FCString::Atof(*atomRecords[i + j + 1]), FCString::Atof(*atomRecords[i + j + 2]));
+
+						//atomPositions.Add(position);
+						//break;
+					}
+				}	
 			}
 		}
+
+		UE_LOG(LogTemp, Warning, TEXT("total atoms: %d"), atomPositions.Num());
 
 	}
 
